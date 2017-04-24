a366e0a8e89006b80cb0fb89f93724a5f954019e
Herminio Garcia <hermano360@gmail.com>
2017-04-24 02:49:24 -0400

    add info to about page and add links to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,22 @@
 var React = require('react');
 
 var About = (props)=> {
-  return (<div>
-    <h3>About</h3>
-    <p>Welcome to the About Page!</p>
+  return (
+  <div>
+    <h1 className="text-center">About</h1>
+    <p>This is a simple React application that can
+    give the current temperature of any listed city</p>
+    <p>Here are some of the tools I used:</p>
+    <ul>
+      <li>
+        <a href="https://facebook.github.io/react">React</a> - This was the JS
+        framework used.
+      </li>
+      <li>
+        <a href="http://openweathermap.org">Open Weather Map</a> - The API used
+         to search for weather data by city name.
+      </li>
+    </ul>
   </div>
 
   )
